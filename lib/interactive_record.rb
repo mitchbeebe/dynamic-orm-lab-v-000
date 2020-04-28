9b411cbcea7bc2c91927aba192990f2b4b1e1514
Mitch Beebe <mitchell.beebe@flatironschool.com>
2020-04-28 01:07:07 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,6 +53,6 @@ class InteractiveRecord
   end
   
   def self.find_by(hash={})
-    DB[:conn].execute("select * from #{self.table_name} where #{hash.keys} = ?", hash.values)
+    DB[:conn].execute("select * from #{self.table_name} where #{hash.keys.to_s} = ?", hash.values)
   end
 end
