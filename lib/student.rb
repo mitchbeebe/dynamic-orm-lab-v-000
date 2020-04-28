0b4ff33014146babcb5a396dd56571e0647be4ed
Mitch Beebe <mitchell.beebe@flatironschool.com>
2020-04-28 00:36:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,6 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  
+  self.class.column_names.each do |prop, val|
+    self.send("#{prop}=", val)
 end
