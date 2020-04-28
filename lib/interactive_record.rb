e9e6592129f683cbf412024c5f49fd0c76423cf6
Mitch Beebe <mitchell.beebe@flatironschool.com>
2020-04-28 00:30:54 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,7 +20,7 @@ class InteractiveRecord
     column_names.compact
   end
   
-  def initialize(options={})
+  def initialize(options)
     options.each do |property, value|
       self.send("#{property}=", value)
     end
